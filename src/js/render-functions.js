67ccda2cd1650f08fe7d7cae5829160734b9cbd5
anna <anytagolenok2@ukr.net>
2026-01-23 11:02:35 +0100

    filled in with css styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,47 +4,56 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
-// Ініціалізація SimpleLightbox для галереї
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
 });
 
-// Створення карток галереї
 export function createGallery(images) {
     const markup = images
+        .slice(0, 9) // обмежуємо 9 картинок
         .map(
             img => `
-      <li class="gallery-item">
-        <a href="${img.largeImageURL}">
-          <img src="${img.webformatURL}" alt="${img.tags}" />
-        </a>
-        <div class="info">
-          <p><b>Likes:</b> ${img.likes}</p>
-          <p><b>Views:</b> ${img.views}</p>
-          <p><b>Comments:</b> ${img.comments}</p>
-          <p><b>Downloads:</b> ${img.downloads}</p>
-        </div>
-      </li>
-    `
+        <li class="image-wrapper">
+          <a href="${img.largeImageURL}">
+            <img src="${img.webformatURL}" alt="${img.tags}" />
+          </a>
+          <div class="stats-panel">
+            <div class="stat">
+              <span class="stat-title">Likes</span>
+              <span class="stat-number">${img.likes}</span>
+            </div>
+            <div class="stat">
+              <span class="stat-title">Views</span>
+              <span class="stat-number">${img.views}</span>
+            </div>
+            <div class="stat">
+              <span class="stat-title">Comments</span>
+              <span class="stat-number">${img.comments}</span>
+            </div>
+            <div class="stat">
+              <span class="stat-title">Downloads</span>
+              <span class="stat-number">${img.downloads}</span>
+            </div>
+          </div>
+        </li>
+      `
         )
         .join('');
 
-    gallery.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh(); // Оновлюємо SimpleLightbox після додавання карток
+    gallery.innerHTML = markup; // додаємо всі картки одночасно
+    lightbox.refresh();
 }
 
-// Очищаємо галерею перед новим пошуком
 export function clearGallery() {
     gallery.innerHTML = '';
 }
 
-// Показуємо лоадер перед запитом
 export function showLoader() {
     loader.classList.remove('hidden');
+    loader.textContent = 'Loading images, please wait...'; // текст лоадера
 }
 
-// Прибираємо лоадер після завершення запиту
 export function hideLoader() {
     loader.classList.add('hidden');
 }
